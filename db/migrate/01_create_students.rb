7db3506ee6b8f12e3ac5f23fd0c62fe54aa6f243
Austin Smith <trunk9slamchest@gmail.com>
2019-09-30 13:30:54 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+    # CREATE TABLE if NOT EXISTS students (
+    #     id INTEGER PRIMARY KEY,
+    #     name TEXT
+    #   );
+  end
 end
